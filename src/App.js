74083d28e4f642595d4e0bd9d3c1fa9c2f0cf59f
Smit <smitsekhadia99@gmail.com>
2022-03-30 01:20:41 +0530

    ispoint within radius ongoing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 import React from "react";
-import { useJsApiLoader, GoogleMap } from "@react-google-maps/api";
+import { useJsApiLoader, GoogleMap,Marker } from "@react-google-maps/api";
 import {useState,useEffect} from 'react';
-
+// import { getDistance,isPointWithinRadius} from 'geolib';
+// import geolib from 'geolib';
+import * as geolib from 'geolib';
 
 function App() {
   const [latitude,setLatitude]=useState(0)
@@ -16,7 +18,7 @@ function App() {
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: "AIzaSyCZRca5LTDbaSaZZwe9WjTk9oGzOskIz-8",
   });
-  const center = { lat: 19.19769277027365, lng: 72.85464173102034 };
+  let center = { lat: 19.19769277027365, lng: 72.85464173102034 };
 
   if (!isLoaded) {
     return <div>NOT LOADED</div>;
@@ -31,7 +33,57 @@ function App() {
     });
   }
   
+  let arr=[{
+    lat:parseFloat(19.19443222884145),
+    lng:parseFloat(72.84613530780784)
+  },
+  {
+    lat:parseFloat(19.20337583618798),
+    lng:parseFloat(72.85956859588623)
+  },
+  {
+    lat:parseFloat(19.19945782353501),
+    lng:parseFloat(72.84604947711937)
+  },
+  {
+    lat:parseFloat(19.20038992529954),
+    lng:parseFloat(72.8624959338949)
+ },
+  {
+    lat:parseFloat(19.193743175937936),
+    lng:parseFloat(72.85545781743983)
+  },
+  {
+    lat:parseFloat(19.20873851048727),
+    lng:parseFloat(72.85335496557215)
+  },
+  {
+    lat:parseFloat(20.506876734232385),
+    lng:parseFloat(73.31951923400982)
+  },
+  {
+    lat:parseFloat(21.07178033055075),
+    lng:parseFloat(73.12176532775982)
+  },
+  {
+    lat:parseFloat(20.928187671260243),
+    lng:parseFloat(73.96771259338482)
+  },
+]
+let nearbyUsers=[];
+// let nearbyUsers = Object.values(arr.map(item => getDistance(center, item)).filter(distance => distance < 5000))
+for(let i=0;i<arr.length;i++)
+{
+  if(geolib.isPointWithinRadius(
+    { latitude: arr[i].lat, longitude: arr[i].lng },
+    { latitude: center.lat, longitude: center.lng },
+    5000))
+  {
+    nearbyUsers=[...arr[i]]
+  }
+}
 
+console.log(nearbyUsers)
   return (
     <div style={{ position: "absolute", width: "100%", height: "100%" }}>
       <GoogleMap
@@ -41,8 +93,19 @@ function App() {
         onClick={(ev) => {
           console.log("latitide = ", ev.latLng.lat());
           console.log("longitude = ", ev.latLng.lng());
+        
         }}
-      ></GoogleMap>
+        options={{
+          streetViewControl:false
+        }}
+      >
+        
+        {nearbyUsers.map((item)=>{
+          return <Marker position={item}>
+          </Marker>
+        })}
+        
+      </GoogleMap>
     </div>
   );
 }
